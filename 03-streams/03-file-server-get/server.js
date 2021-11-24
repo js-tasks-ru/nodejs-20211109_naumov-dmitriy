ac9dcd730351c763c8acf8d0d807acba7e42d5e7
Dmitryi Naumov <naumovdv2010@gmail.com>
2021-11-24 18:03:23 +0400

    3th task by stream - get file from server

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,18 +1,46 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
+  
+  if (pathname.includes('/')) {
+    res.statusCode = '400';
+    res.end('Subfolders are not supported');
+    return;
+  }
 
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
-
+      // проверка на существование файла
+      fs.access(filepath, fs.constants.F_OK, (err) => {
+        if (err) {
+          res.statusCode = 404;
+          res.end('File not found');
+          return;
+        }
+    
+        // отправка файла
+        const stream = fs.createReadStream(filepath);
+        stream.pipe(res);
+    
+        stream.on('error', (error) => {
+          res.statusCode = 500;
+          res.end('Internal server error');
+          return;
+        });
+    
+        req.on('close', function() {
+          stream.destroy();
+        });
+      });      
       break;
 
     default:
